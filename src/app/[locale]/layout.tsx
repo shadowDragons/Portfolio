bd087e135ef584f4dc7564d87763bcfc2f58ca5d
shadowDragons <shadow_dragon@126.com>
2024-12-17 10:40:26 +0800

    first commit

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -83,8 +83,8 @@ export default async function RootLayout({ children, params: { locale } }: { chi
           <Navbar />
           <main
             className={cn(
-              'flex flex-col relative break-words min-h-screen pt-14 pb-4 px-40 max-md:p-4 bg-transparent max-sm:pt-20 bg-[radial-gradient(#2f7df4_1px,transparent_1px)] [background-size:16px_16px]',
-              { 'bg-white': '#E6E7EB' }
+              'flex flex-col relative break-words min-h-screen pt-14 pb-4 px-40 max-md:p-4 bg-transparent max-sm:pt-20',
+              'bg-[linear-gradient(to_right,#e5e7eb_1px,transparent_1px),linear-gradient(to_bottom,#e5e7eb_1px,transparent_1px)] bg-[size:24px_24px]'
             )}
           >
             {children}
